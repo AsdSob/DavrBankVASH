7727845e54bbae0da82f8c9dcc313cac48846ef4
SobirASD <34741911+SobirASD@users.noreply.github.com>
2021-11-02 11:35:54 +0500

    core base authorization
    https://jasonwatmore.com/post/2021/05/25/net-5-simple-api-for-authentication-registration-and-user-management#automapper-profile-cs,
    
    https://github.com/cornflourblue/dotnet-5-registration-login-api

---
+++ DavrBank/DavrBank.AuthorizationApi/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace DavrBank.AuthorizationApi.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ DavrBank/DavrBank.AuthorizationApi/Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+using DavrBank.AuthorizationApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DavrBank.AuthorizationApi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
